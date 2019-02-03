d16256599c9bceff443fb14ad357f614166f8896
Jsiao <jsiao@localhost.localdomain>
2019-02-03 02:26:16 -0800

    libsave 0.1

--- libsave/libsave.h
+++ libsave/libsave.h
@@ -1,6 +1,11 @@
 #pragma once
+#ifndef LIBSAVE
+#define LIBSAVE
 
 #include <stdio.h>
 #include <stdlib.h>
 
+void  struct_save(FILE *, size_t, void *);
+void *struct_load(FILE *, size_t);
 
+#endif
